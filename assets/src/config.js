cc24f8f0e989f0a7e0b3bbc140acff7266ff663e
Safeer Mohiudddin <safeer@safeer.me>
2018-12-03 18:28:58 -0800

    Update config.js
    Updating API keys to sample deployed application.

--- assets/src/config.js
+++ assets/src/config.js
@@ -1,12 +1,12 @@
 export default {
   apiGateway: {
     REGION: "us-east-1",
-    API_URL: "https://nv3qmo0808.execute-api.us-east-1.amazonaws.com/prod"
+    API_URL: "https://yuq7ws39pj.execute-api.us-east-1.amazonaws.com/prod",
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_zj2JBdZmM",
-    APP_CLIENT_ID: "4a0f0icmbka52u5v0nkp5k83m9",
-    IDENTITY_POOL_ID: "us-east-1:5b533eee-3e1b-4666-b47d-eaae095127b6"
+    USER_POOL_ID: "us-east-1_yA0vYd5AL",
+    APP_CLIENT_ID: "25048pu470diqqcvdo56i0cat9",
+    IDENTITY_POOL_ID: "us-east-1:1ff5a5f9-dd44-4934-b575-97d8d0952439"
   }
 };
